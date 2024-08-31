3ba560ecaa307fede284d218c5ee3556960722d2
devitalo <italo_ariel_dev77@outlook.com>
2024-08-31 00:13:05 -0300

    initial commmit

---
+++ ExerPooDio/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExerPooDio.Models;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("nokia");
+
+Smartphone nokia = new Nokia(numero: "12345678", modelo: "novo", imei: "11111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("whatsapp");
+
+Console.WriteLine("iphone");
+
+Smartphone iphone = new Nokia(numero: "12345678", modelo: "novo", imei: "11111", memoria: 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("whatsapp");
